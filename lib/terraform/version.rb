78f2879f88a596b73aa165d3d2ebfb2933fe5ed4
Aaron Lane <aaron.lane@newcontext.com>
2016-09-26 13:34:33 -0400

    Remove rebase errors

--- lib/terraform/version.rb
+++ lib/terraform/version.rb
@@ -14,9 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'kitchen'
-require 'nesty'
-
 module Terraform
   VERSION = '0.3.0'
 end
