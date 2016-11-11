6ea2ee9ae042345e58c94a79dac8c6743a2d5729
shinka81 <clayton.thomas@newcontext.com>
2016-11-11 16:18:31 -0600

    Adding support for discovered InSpec attributes

--- examples/detailed/outputs.tf
+++ examples/detailed/outputs.tf
@@ -5,3 +5,7 @@ output "example_1_public_dns" {
 output "example_2_public_ip" {
   value = "${aws_instance.kitchen_terraform_example_2.public_ip}"
 }
+
+output "security_group" {
+  value = "${aws_security_group.kitchen_terraform_example.name}"
+}
