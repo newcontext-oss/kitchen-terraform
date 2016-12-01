b8df0f755457ea45b699137dc218e343ea5a3985
Aaron Lane <aaron.lane@newcontext.com>
2016-12-01 11:49:11 -0800

    Update main and aws_provider READMEs

--- examples/aws_provider/example.tf
+++ examples/aws_provider/example.tf
@@ -1,6 +1,4 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
   region     = "${var.region}"
 }
 
--- examples/aws_provider/test/fixtures/0.7/variables.tf
+++ examples/aws_provider/test/fixtures/0.7/variables.tf
@@ -1,8 +1,3 @@
-variable "access_key" {
-  type        = "string"
-  description = "AWS access key ID"
-}
-
 variable "public_key_pathname" {
   type        = "string"
   description = "Pathname of SSH public key"
@@ -12,9 +7,4 @@ variable "region" {
   type        = "string"
   description = "AWS region"
   default     = "us-east-1"
-}
-
-variable "secret_key" {
-  type        = "string"
-  description = "AWS secret access key"
 }
