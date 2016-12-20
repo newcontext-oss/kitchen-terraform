15ff67f083afe0a71f5e1d6fe452cb90094faa4e
Aaron Lane <aaron.lane@newcontext.com>
2016-12-20 14:33:33 -0500

    Fix parentheses spacing issue

--- examples/docker_provider/test/integration/default/controls/operating_system.rb
+++ examples/docker_provider/test/integration/default/controls/operating_system.rb
@@ -4,6 +4,6 @@ control 'operating_system' do
   describe 'the operating system' do
     subject { command('lsb_release -a').stdout }
 
-    it('is Ubuntu') { is_expected.to match (/Ubuntu/) }
+    it('is Ubuntu') { is_expected.to match(/Ubuntu/) }
   end
 end
