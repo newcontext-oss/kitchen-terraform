ad264ae5c2514cf34f2b4603b2c236706788b979
Aaron Lane <aaron.lane@newcontext.com>
2016-12-20 14:56:07 -0500

    Add Docker provider example to Travis CI (#69)
    * Add integration tests to Travis
    
    * Reset directory after integration test work
    
    * Install Terraform
    
    * Fix curl output flag
    
    * Explicitly export PATH
    
    * Move PATH to integration test command
    
    * Depend docker_container on docker_image
    
    * Fix style issues in operating_system control
    
    * Fix parentheses spacing issue

--- examples/docker_provider/test/integration/default/controls/operating_system.rb
+++ examples/docker_provider/test/integration/default/controls/operating_system.rb
@@ -1,1 +1,9 @@
-describe command('lsb_release -a') do its('stdout') { should match (/Ubuntu/) } end
+# frozen_string_literal: true
+
+control 'operating_system' do
+  describe 'the operating system' do
+    subject { command('lsb_release -a').stdout }
+
+    it('is Ubuntu') { is_expected.to match(/Ubuntu/) }
+  end
+end
