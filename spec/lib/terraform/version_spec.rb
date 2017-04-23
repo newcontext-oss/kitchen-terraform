63cf88d0b0407bf0437be1ce195470b8c63badcf
Aaron Lane <aaron.lane@newcontext.com>
2017-04-22 20:06:23 -0400

    Revert "Update copyright year to 2017"
    This reverts commit 8f59ce4158f53a392d7436b4582d715048919581.

--- spec/lib/terraform/version_spec.rb
+++ spec/lib/terraform/version_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Copyright 2016-2017 New Context Services, Inc.
+# Copyright 2016 New Context Services, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
