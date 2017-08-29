a0314b1e22d005bc19532c56714a70b4ef2ef11c
Aaron Lane <aaron.lane@newcontext.com>
2017-08-29 09:47:23 -0400

    Refactor client/options_spec

--- spec/lib/kitchen/terraform/client/options_spec.rb
+++ spec/lib/kitchen/terraform/client/options_spec.rb
@@ -17,396 +17,359 @@
 require "kitchen/terraform/client/options"
 
 ::RSpec.describe ::Kitchen::Terraform::Client::Options do
-  let :described_instance do
-    described_class.new
-  end
-
-  describe ".backend_config" do
-    subject do
-      described_instance
-        .backend_config(
-          key: "key",
-          value: "value"
-        )
-        .to_s
-    end
-
+  shared_examples "a bulk mapper" do |method:, key:|
     it do
-      is_expected.to eq "-backend-config='key=value'"
+      expect(
+        subject
+          .send(
+            method,
+            keys_and_values: {
+              key_1: "value_1",
+              key_2: "value_2"
+            }
+          )
+          .to_s
+      )
+        .to eq "#{key}='key_1=value_1' #{key}='key_2=value_2'"
     end
   end
 
-  describe ".backend_configs" do
-    subject do
-      described_instance
-        .backend_configs(
-          keys_and_values: {
-            key_1: :value_1,
-            key_2: :value_2
-          }
-        )
-        .to_s
-    end
-
+  describe "#backend_config" do
     it do
-      is_expected.to eq "-backend-config='key_1=value_1' -backend-config='key_2=value_2'"
+      expect(
+        subject
+          .backend_config(
+            key: "key",
+            value: "value"
+          )
+          .to_s
+      )
+        .to eq "-backend-config='key=value'"
     end
   end
 
-  describe ".backup" do
-    subject do
-      described_instance
-        .backup(path: "/path")
-        .to_s
-    end
+  describe "#backend_configs" do
+    it_behaves_like(
+      "a bulk mapper",
+      method: :backend_configs,
+      key: "-backend-config"
+    )
+  end
 
+  describe "#backup" do
     it do
-      is_expected.to eq "-backup=/path"
+      expect(
+        subject
+          .backup(path: "/path")
+          .to_s
+      )
+        .to eq "-backup=/path"
     end
   end
 
-  describe ".disable_input" do
-    subject do
-      described_instance
-        .disable_input
-        .to_s
-    end
-
+  describe "#disable_input" do
     it do
-      is_expected.to eq "-input=false"
+      expect(
+        subject
+          .disable_input
+          .to_s
+      )
+        .to eq "-input=false"
     end
   end
 
-  describe ".disable_verify_plugins" do
-    subject do
-      described_instance
-        .disable_verify_plugins
-        .to_s
-    end
-
+  describe "#disable_verify_plugins" do
     it do
-      is_expected.to eq "-verify-plugins=false"
+      expect(
+        subject
+          .disable_verify_plugins
+          .to_s
+      )
+        .to eq "-verify-plugins=false"
     end
   end
 
-  describe ".enable_auto_approve" do
-    subject do
-      described_instance
+  describe "#enable_auto_approve" do
+    it do
+      expect(
+        subject
         .enable_auto_approve
         .to_s
-    end
-
-    it do
-      is_expected.to eq "-auto-approve=true"
+      )
+        .to eq "-auto-approve=true"
     end
   end
 
-  describe ".enable_backend" do
-    subject do
-      described_instance
-        .enable_backend
-        .to_s
-    end
-
+  describe "#enable_backend" do
     it do
-      is_expected.to eq "-backend=true"
+      expect(
+        subject
+          .enable_backend
+          .to_s
+      )
+        .to eq "-backend=true"
     end
   end
 
-  describe ".enable_check_variables" do
-    subject do
-      described_instance
+  describe "#enable_check_variables" do
+    it do
+      expect(
+        subject
         .enable_check_variables
         .to_s
-    end
-
-    it do
-      is_expected.to eq "-check-variables=true"
+      )
+        .to eq "-check-variables=true"
     end
   end
 
-  describe ".enable_get" do
-    subject do
-      described_instance
-        .enable_get
-        .to_s
-    end
-
+  describe "#enable_get" do
     it do
-      is_expected.to eq "-get=true"
+      expect(
+        subject
+          .enable_get
+          .to_s
+      )
+        .to eq "-get=true"
     end
   end
 
-  describe ".enable_lock" do
-    subject do
-      described_instance
-        .enable_lock
-        .to_s
-    end
-
+  describe "#enable_lock" do
     it do
-      is_expected.to eq "-lock=true"
+      expect(
+        subject
+          .enable_lock
+          .to_s
+      )
+        .to eq "-lock=true"
     end
   end
 
-  describe ".enable_refresh" do
-    subject do
-      described_instance
-        .enable_refresh
-        .to_s
-    end
-
+  describe "#enable_refresh" do
     it do
-      is_expected.to eq "-refresh=true"
+      expect(
+        subject
+          .enable_refresh
+          .to_s
+      )
+        .to eq "-refresh=true"
     end
   end
 
-  describe ".force" do
-    subject do
-      described_instance
-        .force
-        .to_s
-    end
-
+  describe "#force" do
     it do
-      is_expected.to eq "-force"
+      expect(
+        subject
+          .force
+          .to_s
+      )
+        .to eq "-force"
     end
   end
 
-  describe ".force_copy" do
-    subject do
-      described_instance
-        .force_copy
-        .to_s
-    end
-
+  describe "#force_copy" do
     it do
-      is_expected.to eq "-force-copy"
+      expect(
+        subject
+          .force_copy
+          .to_s
+      )
+        .to eq "-force-copy"
     end
   end
 
-  describe ".from_module" do
-    subject do
-      described_instance
-        .from_module(source: "/source")
-        .to_s
-    end
-
+  describe "#from_module" do
     it do
-      is_expected.to eq "-from-module=/source"
+      expect(
+        subject
+          .from_module(source: "/source")
+          .to_s
+      )
+        .to eq "-from-module=/source"
     end
   end
 
-  describe ".json" do
-    subject do
-      described_instance
-        .json
-        .to_s
-    end
-
+  describe "#json" do
     it do
-      is_expected.to eq "-json"
+      expect(
+        subject
+          .json
+          .to_s
+      )
+        .to eq "-json"
     end
   end
 
-  describe ".lock_timeout" do
-    subject do
-      described_instance
-        .lock_timeout(duration: "1m")
-        .to_s
-    end
-
+  describe "#lock_timeout" do
     it do
-      is_expected.to eq "-lock-timeout=1m"
+      expect(
+        subject
+          .lock_timeout(duration: "1m")
+          .to_s
+      )
+        .to eq "-lock-timeout=1m"
     end
   end
 
-  describe ".maybe_no_color" do
-    subject do
-      described_instance
-        .maybe_no_color(toggle: toggle)
-        .to_s
-    end
-
+  describe "#maybe_no_color" do
     context "when the toggle is truthy" do
-      let :toggle do
-        true
-      end
-
       it do
-        is_expected.to eq "-no-color"
+        expect(
+          subject
+            .maybe_no_color(toggle: true)
+            .to_s
+        )
+          .to eq "-no-color"
       end
     end
 
     context "when the toggle is falsey" do
-      let :toggle do
-        false
-      end
-
       it do
-        is_expected.to eq ""
+        expect(
+          subject
+            .maybe_no_color(toggle: false)
+            .to_s
+        )
+          .to eq ""
       end
     end
   end
 
-  describe ".maybe_plugin_dir" do
-    subject do
-      described_instance
-        .maybe_plugin_dir(path: path)
-        .to_s
-    end
-
+  describe "#maybe_plugin_dir" do
     context "when the path is truthy" do
-      let :path do
-        "/path"
-      end
-
       it do
-        is_expected.to eq "-plugin-dir=/path"
+        expect(
+          subject
+            .maybe_plugin_dir(path: "/path")
+            .to_s
+        )
+          .to eq "-plugin-dir=/path"
       end
     end
 
     context "when the path is falsey" do
-      let :path do
-        nil
-      end
-
       it do
-        is_expected.to eq ""
+        expect(
+          subject
+            .maybe_plugin_dir(path: nil)
+            .to_s
+        )
+          .to eq ""
       end
     end
   end
 
-  describe ".no_color" do
-    subject do
-      described_instance
-        .no_color
-        .to_s
-    end
-
+  describe "#no_color" do
     it do
-      is_expected.to eq "-no-color"
+      expect(
+        subject
+          .no_color
+          .to_s
+      )
+        .to eq "-no-color"
     end
   end
 
-  describe ".parallelism" do
-    subject do
-      described_instance
-        .parallelism(concurrent_operations: 123)
-        .to_s
-    end
-
+  describe "#parallelism" do
     it do
-      is_expected.to eq "-parallelism=123"
+      expect(
+        subject
+          .parallelism(concurrent_operations: 123)
+          .to_s
+      )
+        .to eq "-parallelism=123"
     end
   end
 
-  describe ".plugin_dir" do
-    subject do
-      described_instance
-        .plugin_dir(path: "/path")
-        .to_s
-    end
-
+  describe "#plugin_dir" do
     it do
-      is_expected.to eq "-plugin-dir=/path"
+      expect(
+        subject
+          .plugin_dir(path: "/path")
+          .to_s
+      )
+        .to eq "-plugin-dir=/path"
     end
   end
 
-  describe ".state" do
-    subject do
-      described_instance
-        .state(path: "/path")
-        .to_s
-    end
-
+  describe "#state" do
     it do
-      is_expected.to eq "-state=/path"
+      expect(
+        subject
+          .state(path: "/path")
+          .to_s
+      )
+        .to eq "-state=/path"
     end
   end
 
-  describe ".state_out" do
-    subject do
-      described_instance
-        .state_out(path: "/path")
-        .to_s
-    end
-
+  describe "#state_out" do
     it do
-      is_expected.to eq "-state-out=/path"
+      expect(
+        subject
+          .state_out(path: "/path")
+          .to_s
+      )
+        .to eq "-state-out=/path"
     end
   end
 
-  describe ".upgrade" do
-    subject do
-      described_instance
-        .upgrade
-        .to_s
-    end
-
+  describe "#upgrade" do
     it do
-      is_expected.to eq "-upgrade"
+      expect(
+        subject
+          .upgrade
+          .to_s
+      )
+        .to eq "-upgrade"
     end
   end
 
-  describe ".var" do
-    subject do
-      described_instance
-        .var(
-          key: "key",
-          value: "value"
-        )
-        .to_s
-    end
-
+  describe "#var" do
     it do
-      is_expected.to eq "-var='key=value'"
+      expect(
+        subject
+          .var(
+            key: "key",
+            value: "value"
+          )
+          .to_s
+      )
+        .to eq "-var='key=value'"
     end
   end
 
-  describe ".vars" do
-    subject do
-      described_instance
-        .vars(
-          keys_and_values: {
-            key_1: :value_1,
-            key_2: :value_2
-          }
-        )
-        .to_s
-    end
+  describe "#vars" do
+    it_behaves_like(
+      "a bulk mapper",
+      key: "-var",
+      method: :vars
+    )
+  end
 
+  describe "#var_file" do
     it do
-      is_expected.to eq "-var='key_1=value_1' -var='key_2=value_2'"
+      expect(
+        subject
+          .var_file(path: "/path")
+          .to_s
+      )
+        .to eq "-var-file=/path"
     end
   end
 
-  describe ".var_file" do
-    subject do
-      described_instance
-        .var_file(path: "/path")
-        .to_s
-    end
-
+  describe "#var_files" do
     it do
-      is_expected.to eq "-var-file=/path"
-    end
-  end
-
-  describe ".var_files" do
-    subject do
-      described_instance
-        .var_files(
-          paths: [
-            "/path_1",
-            "/path_2"
-          ]
-        )
-        .to_s
-    end
-
-    it do
-      is_expected.to eq "-var-file=/path_1 -var-file=/path_2"
+      expect(
+        subject
+          .var_files(
+            paths: [
+              "/path_1",
+              "/path_2"
+            ]
+          )
+          .to_s
+      )
+        .to eq "-var-file=/path_1 -var-file=/path_2"
     end
   end
 end
