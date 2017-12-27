177bb13ccbabe8e5f86476e06b3ed1f477ee9998
Aaron Lane <aaron.lane@newcontext.com>
2017-12-27 17:30:22 -0500

    Clarify documentation of groups attribute
    - Adjust style to be consistent with other attributes
    - Adjust headers to be smaller than h4 used in verifier document to
      introduce this attribute
    - Expand examples
    
    [skip ci]

--- lib/kitchen/terraform/config_attribute/groups.rb
+++ lib/kitchen/terraform/config_attribute/groups.rb
@@ -19,30 +19,29 @@ require "kitchen/terraform/config_attribute_cacher"
 require "kitchen/terraform/config_attribute_definer"
 require "kitchen/terraform/config_schemas/groups"
 
-# This attribute is an optional array including hashes comprising properties to manage the execution of
-# {https://www.inspec.io/docs/reference/profiles/ InSpec profiles} against different resources in the Terraform state.
+# This attribute configures the execution of {https://www.inspec.io/docs/reference/profiles/ InSpec profiles} against
+# different groups of resources in the Terraform state.
 #
 # Type:: {http://www.yaml.org/spec/1.2/spec.html#id2760193 Sequence of mappings}
 # Required:: False
+#
+# ===== Mapping Keys
+#
+# These keys may be declared in each of the mappings in the sequence.
+#
+# ====== name
+#
+# This key contains the name of the group to be used for logging purposes.
+#
+# Type:: {http://www.yaml.org/spec/1.2/spec.html#id2760844 Scalar}
+# Required:: True
 # Example::
 #   _
 #     groups:
 #       -
 #         name: a_group
 #
-# === Mapping Keys
-#
-# These keys must be declared in each of the mappings in the sequence.
-#
-# ==== name
-#
-# This key contains the name of the group to be used for logging purposes.
-#
-# Type:: {http://www.yaml.org/spec/1.2/spec.html#id2760844 Scalar}
-# Required:: True
-# Example:: <code>name: a_group</code>
-#
-# ==== attributes
+# ====== attributes
 #
 # This key comprises associations of the names of
 # {https://www.inspec.io/docs/reference/profiles/#profile-attributes InSpec profile attributes} with the names of
@@ -53,61 +52,92 @@ require "kitchen/terraform/config_schemas/groups"
 # Required:: False
 # Example::
 #   _
-#     attributes:
-#       an_attribute: an_output
+#     groups:
+#       -
+#         name: a_group_with_overridden_attributes
+#         attributes:
+#           an_attribute: an_output
 # Caveat:: As all Terraform outputs are associated with equivalently named InSpec profile attributes by default, this
 #          key is only necessary to provide alternative attribute names.
 #
-# ==== controls
+# ====== controls
 #
 # This key comprises the names of {https://www.inspec.io/docs/reference/dsl_inspec/ InSpec controls} to exclusively
-# include from the InSpec profile of the Test Kitchen suite.
+# include from the InSpec profile of the associated Test Kitchen instance.
 #
 # Type:: {http://www.yaml.org/spec/1.2/spec.html#id2760118 Sequince of scalars}
 # Required:: False
 # Example::
 #   _
-#     controls:
-#       - first_control
-#       - third_control
+#     groups:
+#       -
+#         name: a_group_with_a_first_subset_of_controls
+#         controls:
+#           - control_one
+#           - control_three
+#       -
+#         name: a_group_with_a_second_subset_of_controls
+#         controls:
+#           - control_two
+#           - control_four
 #
-# ==== hostnames
+# ====== hostnames
 #
-# This key contains the name of a Terraform output.
+# This key contains the name of a Terraform output which provides one or more hostnames to be targeted by the InSpec
+# profile of the associated Test Kitchen instance.
 #
 # Type:: {http://www.yaml.org/spec/1.2/spec.html#id2760844 Scalar}
 # Required:: False
-# Example:: <code>hostnames: an_output</code>
-# Caveat:: The output must be of type String or Array and must contain one or more hostnames that will be the targets of
-#          the InSpec profile of the Test Kitchen suite. If this key is omitted then +"localhost"+ will be the target of
-#          the profile.
+# Example::
+#   _
+#     groups:
+#       -
+#         name: a_group_with_hostnames
+#         hostnames: an_output
+# Caveat:: The output must be of type String or Array and . If this key is omitted then +"localhost"+ will be the target
+#          of the profile.
 #
-# ==== port
+# ====== port
 #
 # This key contains the port to use when connecting with {https://en.wikipedia.org/wiki/Secure_Shell Secure Shell (SSH)}
 # to the hosts of the group.
 #
 # Type:: {http://www.yaml.org/spec/1.2/spec.html#id2803828 Integer}
 # Required:: False
-# Example:: <code>port: 1234</code>
+# Example::
+#   _
+#     groups:
+#       -
+#         name: a_group_with_a_port
+#         port: 1234
 # Caveat:: If this key is omitted then the port of the Test Kitchen SSH transport will be used.
 #
-# ==== ssh_key
+# ====== ssh_key
 #
 # This key contains the path to a private SSH key to use when connecting with SSH to the hosts of the group.
 #
 # Type:: {http://www.yaml.org/spec/1.2/spec.html#id2760844 Scalar}
 # Required:: False
-# Example:: <code>ssh_key: /path/to/an/ssh/key</code>
+# Example::
+#   _
+#     groups:
+#       -
+#         name: a_group_with_an_ssh_key
+#         ssh_key: /path/to/an/ssh/key</
 # Caveat:: If this key is omitted then the private SSH key of the Test Kitchen SSH Transport will be used.
 #
-# ==== username
+# ====== username
 #
 # This key contains the username to use when connecting with SSH to the hosts of the group.
 #
 # Type:: {http://www.yaml.org/spec/1.2/spec.html#id2760844 Scalar}
 # Required:: False
-# Example:: <code>ssh_key: /path/to/an/ssh/key</code>
+# Example::
+#   _
+#     groups:
+#       -
+#         name: a_group_with_a_username
+#         username: tester
 # Caveat:: If this key is omitted then the username of the Test Kitcen SSH Transport will be used.
 #
 # @abstract It must be included by a plugin class in order to be used.
