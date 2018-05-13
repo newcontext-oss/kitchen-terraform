c9fd71d66a296f67757099c24cfde1acd5a2e7d5
Aaron Lane <aaron.lane@newcontext.com>
2018-05-13 15:07:11 -0400

    Document and specify Kitchen::Terraform::Version

--- lib/kitchen/terraform/version.rb
+++ lib/kitchen/terraform/version.rb
@@ -17,45 +17,66 @@
 require "rubygems"
 require "kitchen/terraform"
 
-# This class represents the version of the Kitchen-Terraform gem.
+# Kitchen::Terraform::Version represents the version of the Kitchen-Terraform gem. The module can send the version to
+# different containers as well as conditionally yield to blocks based on version requirements.
 module ::Kitchen::Terraform::Version
-  # rubocop:disable Style/MutableConstant
-  self::VALUE = "3.3.1"
-  # rubocop:enable Style/MutableConstant
-
-  def self.assign_specification_version(specification:)
-    specification.version = value.to_s
-    self
-  end
-
+  # assign_plugin_version assigns the version to a class which includes Kitchen::Configurable.
+  #
+  # @param configurable_class [::Kitchen::Configurable] the configurable class to which the version will be assigned.
+  # @return [self]
   def self.assign_plugin_version(configurable_class:)
     configurable_class.plugin_version value.to_s
     self
   end
 
+  # assign_specification_version assigns the version to a Gem::Specification.
+  #
+  # @param specification [::Gem::Specification] the specification to which the version will be assigned.
+  # @return [self]
+  def self.assign_specification_version(specification:)
+    specification.version = value
+    self
+  end
+
+  # if_satisfies yields control if the provided requirement is satisfied by the version.
+  #
+  # @param requirement [::Gem::Requirement, ::String] the requirement to be satisfied by the version.
+  # @raise [::Gem::Requirement::BadRequirementError] if the requirement is illformed.
+  # @return [self]
+  # @yield [] if the requirement is satisfied by the version.
   def self.if_satisfies(requirement:)
-    ::Gem::Requirement
-      .new(requirement)
-      .satisfied_by? value and
-      yield
+    yield if
+      ::Gem::Requirement
+        .new(requirement)
+        .satisfied_by? value
 
     self
   end
 
+  # temporarily_override overrides the current version with the version provided, yields control, and then resets the
+  # version.
+  #
+  # @note temporarily_override must only be used in tests to validate version flow control logic.
+  # @raise [::ArgumentError] if the version is malformed.
+  # @return [self]
+  # @yield [] the value of the version will be overridden while control is yielded.
   def self.temporarily_override(version:)
     current_value = value
     self.value = version
     yield
     self.value = current_value
+    self
   end
 
   private_class_method
 
+  # @api private
   def self.value
-    self.value = self::VALUE if not @value
+    self.value = ::Gem::Version.new "3.3.1" if not @value
     @value
   end
 
+  # @api private
   def self.value=(version)
     @value = ::Gem::Version.new version
     self
