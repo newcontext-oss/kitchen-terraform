07974a138d6516869eab853c098fdb9bbc35af01
Aaron Lane <aaron.lane@newcontext.com>
2018-06-11 11:15:52 -0400

    Standardize structure of basic suite

---
+++ integration/basic/outputs.tf
@@ -0,0 +1,9 @@
+output "backend_state" {
+  description = "The path to the backend state file"
+  value       = "${path.module}/terraform.tfstate.d/${terraform.workspace}/terraform.tfstate"
+}
+
+output "hostnames" {
+  description = "The hostnames to test"
+  value       = ["localhost"]
+}
