1fe4bc507f9d0973ad65703ac21820cce5eaa32b
Aaron Lane <aaron.lane@newcontext.com>
2018-07-17 10:35:13 -0400

    Refactor attrs control

--- integration/basic/test/integration/example/controls/attrs.rb
+++ integration/basic/test/integration/example/controls/attrs.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
 first_attribute = attribute "first",
-  default: "first_attrs.yml was not loaded", description: "This value should be loaded from test/integration/example/first_attrs.yml."
+                            default: "first_attrs.yml was not loaded",
+                            description: "This value should be loaded from test/integration/example/first_attrs.yml."
 
 second_attribute = attribute "second",
-  default: "second_attrs.yml was not loaded",
-  description: "This value should be loaded from test/integration/example/second_attrs.yml."
+                             default: "second_attrs.yml was not loaded",
+                             description: "This value should be loaded from test/integration/example/second_attrs.yml."
 
 control "attrs" do
   desc "This control validates that the elements of the verifier.groups.x.attrs attribute are successfully passed to " \
-    "InSpec as attrs."
+       "InSpec as attrs."
 
   describe first_attribute do
     it do
