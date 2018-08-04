0f848e99cb0c2ef81e6234dccda45ca2882f1433
Aaron Lane <aaron.lane@newcontext.com>
2018-08-04 17:23:20 -0400

    Format provisioner specification

--- spec/lib/kitchen/provisioner/terraform_spec.rb
+++ spec/lib/kitchen/provisioner/terraform_spec.rb
@@ -20,97 +20,93 @@ require "kitchen/provisioner/terraform"
 require "kitchen/terraform/error"
 require "support/kitchen/terraform/configurable_examples"
 
-::RSpec
-  .describe ::Kitchen::Provisioner::Terraform do
-    let :described_instance do
-      described_class.new({})
+::RSpec.describe ::Kitchen::Provisioner::Terraform do
+  let :config do
+    {}
+  end
+
+  let :described_instance do
+    described_class.new config
+  end
+
+  it_behaves_like "Kitchen::Terraform::Configurable"
+
+  describe "#call" do
+    subject do
+      lambda do
+        described_instance.call kitchen_state
+      end
     end
 
-    it_behaves_like "Kitchen::Terraform::Configurable"
-
-    describe "#call" do
-      subject do
-        lambda do
-          described_instance.call kitchen_state
-        end
-      end
-
-      let :driver do
-        ::Kitchen::Driver::Terraform.new
-      end
-
+    let :driver do
+      ::Kitchen::Driver::Terraform.new
+    end
 
     let :kitchen_instance do
-      ::Kitchen::Instance
-        .new(
-          driver: driver,
-          lifecycle_hooks: ::Kitchen::LifecycleHooks.new({}),
-          logger: ::Kitchen::Logger.new,
-          platform: ::Kitchen::Platform.new(name: "test-platform"),
-          provisioner: described_instance,
-          state_file: ::Kitchen::StateFile
-            .new(
-              "/kitchen/root",
-              "test-suite-test-platform"
-            ),
-          suite: ::Kitchen::Suite.new(name: "test-suite"),
-          transport: ::Kitchen::Transport::Base.new,
-          verifier: ::Kitchen::Verifier::Base.new,
-        )
+      ::Kitchen::Instance.new driver: driver, lifecycle_hooks: ::Kitchen::LifecycleHooks.new(config),
+                              logger: ::Kitchen::Logger.new, platform: ::Kitchen::Platform.new(name: "test-platform"),
+                              provisioner: described_instance,
+                              state_file: ::Kitchen::StateFile.new("/kitchen/root", "test-suite-test-platform"),
+                              suite: ::Kitchen::Suite.new(name: "test-suite"),
+                              transport: ::Kitchen::Transport::Base.new, verifier: ::Kitchen::Verifier::Base.new
     end
 
-      before do
-        described_instance.finalize_config! kitchen_instance
-      end
+    let :kitchen_state do
+      {}
+    end
 
-      describe "error handling" do
-        context "when the driver create action is a failure" do
-          before do
-            allow(driver)
-              .to(
-                receive(:apply)
-                  .and_raise(
-                    ::Kitchen::Terraform::Error,
-                    "mocked Driver#create failure"
-                  )
-              )
-          end
+    before do
+      described_instance.finalize_config! kitchen_instance
+    end
 
-          it do
-            is_expected
-              .to(
-                raise_error(
-                  ::Kitchen::ActionFailed,
+    describe "error handling" do
+      context "when the driver create action is a failure" do
+        before do
+          allow(driver)
+            .to(
+              receive(:apply)
+                .and_raise(
+                  ::Kitchen::Terraform::Error,
                   "mocked Driver#create failure"
                 )
-              )
-          end
-        end
-
-        context "when the driver create action is a success" do
-          before do
-            allow(driver).to receive(:apply).and_yield output: "mocked Driver#create output"
-          end
-
-          it do
-            is_expected.to_not raise_error
-          end
-        end
-      end
-
-      describe "Test Kitchen state manipulation" do
-        subject do
-          kitchen_state
-        end
-
-        before do
-          allow(driver).to receive(:apply).and_yield output: "mocked Driver#create output"
-          described_instance.call kitchen_state
+            )
         end
 
         it do
-          is_expected.to include kitchen_terraform_outputs: "mocked Driver#create output"
+          is_expected
+            .to(
+              raise_error(
+                ::Kitchen::ActionFailed,
+                "mocked Driver#create failure"
+              )
+            )
+        end
+      end
+
+      context "when the driver create action is a success" do
+        before do
+          allow(driver).to receive(:apply).and_yield output: "mocked Driver#create output"
+        end
+
+        it do
+          is_expected.to_not raise_error
         end
       end
     end
+
+    describe "Test Kitchen state manipulation" do
+      subject do
+        kitchen_state
+      end
+
+      before do
+        allow(driver).to receive(:apply).and_yield output: "mocked Driver#create output"
+        described_instance.call kitchen_state
+      end
+
+      it do
+        is_expected.to include kitchen_terraform_outputs: "mocked Driver#create output"
+      end
+    end
   end
+end
