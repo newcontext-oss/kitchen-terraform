ce17c83fc2cf8ef3adab6c947ae693c2e4b6213a
Aaron Lane <aaron.lane@newcontext.com>
2018-08-11 17:14:18 -0400

    fixup previous commit

--- lib/kitchen/verifier/terraform.rb
+++ lib/kitchen/verifier/terraform.rb
@@ -19,203 +19,164 @@ require "kitchen/terraform/config_attribute/color"
 require "kitchen/terraform/config_attribute/systems"
 require "kitchen/terraform/configurable"
 require "kitchen/terraform/error"
-require "kitchen/terraform/system_and_hosts_enumerator"
 require "kitchen/terraform/inspec_options_mapper"
+require "kitchen/terraform/system_attrs_resolver"
+require "kitchen/terraform/system_hosts_resolver"
 
-# This namespace is defined by Kitchen.
-#
-# @see http://www.rubydoc.info/gems/test-kitchen/Kitchen/Verifier
-module ::Kitchen::Verifier
-end
-
-# The verifier utilizes the {https://www.inspec.io/ InSpec infrastructure testing framework} to verify the behaviour and
-# state of resources in the Terraform state.
-#
-# === Commands
-#
-# The following command-line commands are provided by the verifier.
-#
-# ==== kitchen verify
-#
-# A Test Kitchen instance is verified by iterating through the systems and executing the associated InSpec controls in a
-# manner similar to the following command-line command.
-#
-#   inspec exec \
-#     [--attrs=system.attrs] \
-#     --backend=system.backend \
-#     [--backend-cache=system.backend_cache] \
-#     [--no-color] \
-#     [--controls=system.controls] \
-#     [--enable-password=system.enable_password] \
-#     --host=system.hosts_output.x \
-#     [--key-files=system.key_files] \
-#     [--password=system.password] \
-#     [--path=system.path] \
-#     [--port=system.port] \
-#     [--profiles-path=test/integration/suite] \
-#     [--user=system.user]
-#
-# === Configuration Attributes
-#
-# The configuration attributes of the verifier control the behaviour of the InSpec runner. Within the
-# {http://kitchen.ci/docs/getting-started/kitchen-yml Test Kitchen configuration file}, these attributes must be
-# declared in the +verifier+ mapping along with the plugin name.
-#
-#   verifier:
-#     name: terraform
-#     a_configuration_attribute: some value
-#
-# ==== color
-#
-# {include:Kitchen::Terraform::ConfigAttribute::Color}
-#
-# ==== systems
-#
-# {include:Kitchen::Terraform::ConfigAttribute::Sysetms}
-#
-# === Ruby Interface
-#
-# This class implements the interface of Kitchen::Configurable which requires the following Reek suppressions:
-# :reek:PrimaDonnaMethod { exclude: [ finalize_config!, load_needed_dependencies! ] }
-class ::Kitchen::Verifier::Terraform
-  include ::Kitchen::Configurable
-  include ::Kitchen::Logging
-  include ::Kitchen::Terraform::ConfigAttribute::Color
-  include ::Kitchen::Terraform::ConfigAttribute::Systems
-  include ::Kitchen::Terraform::Configurable
-  @api_version = 2
-
-  # The verifier enumerates through each hostname of each system and verifies the associated InSpec controls.
+module Kitchen
+  # This namespace is defined by Kitchen.
   #
-  # @example
-  #   `kitchen verify suite-name`
-  # @param kitchen_state [::Hash] the mutable instance and verifier state.
-  # @raise [::Kitchen::ActionFailed] if the result of the action is a failure.
-  # @return [void]
-  def call(kitchen_state)
-    load_outputs kitchen_state: kitchen_state
-    ::Kitchen::Terraform::SystemAndHostsEnumerator.new(systems: config_systems, outputs: outputs)
-      .each_system_and_hosts do |system:, host:|
-      verify system: system, host: host
+  # @see https://www.rubydoc.info/gems/test-kitchen/Kitchen/Verifier
+  module Verifier
+    # The verifier utilizes the {https://www.inspec.io/ InSpec infrastructure testing framework} to verify the behaviour and
+    # state of resources in the Terraform state.
+    #
+    # === Commands
+    #
+    # The following command-line commands are provided by the verifier.
+    #
+    # ==== kitchen verify
+    #
+    # A Test Kitchen instance is verified by iterating through the systems and executing the associated InSpec controls
+    # against the hosts of each system.
+    #
+    # === Configuration Attributes
+    #
+    # The configuration attributes of the verifier control the behaviour of the InSpec runner. Within the
+    # {http://kitchen.ci/docs/getting-started/kitchen-yml Test Kitchen configuration file}, these attributes must be
+    # declared in the +verifier+ mapping along with the plugin name.
+    #
+    #   verifier:
+    #     name: terraform
+    #     a_configuration_attribute: some value
+    #
+    # ==== color
+    #
+    # {include:Kitchen::Terraform::ConfigAttribute::Color}
+    #
+    # ==== systems
+    #
+    # {include:Kitchen::Terraform::ConfigAttribute::Systems}
+    #
+    # === Ruby Interface
+    #
+    # This class implements the interface of Kitchen::Configurable which requires the following Reek suppressions:
+    # :reek:PrimaDonnaMethod { exclude: [ finalize_config!, load_needed_dependencies! ] }
+    class Terraform
+      include ::Kitchen::Configurable
+      include ::Kitchen::Logging
+      include ::Kitchen::Terraform::ConfigAttribute::Color
+      include ::Kitchen::Terraform::ConfigAttribute::Systems
+      include ::Kitchen::Terraform::Configurable
+      @api_version = 2
+
+      # The verifier enumerates through each host of each system and verifies the associated InSpec controls.
+      #
+      # @example
+      #   `kitchen verify suite-name`
+      # @param kitchen_state [::Hash] the mutable instance and verifier state.
+      # @raise [::Kitchen::ActionFailed] if the result of the action is a failure.
+      # @return [void]
+      def call(kitchen_state)
+        load_outputs kitchen_state: kitchen_state
+        config_systems.each do |system|
+          ::Kitchen::Terraform::InSpecOptionsMapper.new(system: system).map options: @inspec_options
+          ::Kitchen::Terraform::System
+            .new(mapping: system)
+            .resolve_attrs(system_attrs_resolver: @system_attrs_resolver)
+            .resolve_hosts(system_hosts_resolver: @system_hosts_resolver)
+            .verify(inspec_options: @inspec_options, inspec_profile_path: inspec_profile_path)
+        end
+      rescue ::Kitchen::Terraform::Error => error
+        raise ::Kitchen::ActionFailed, error.message
+      end
+
+      # doctor checks the system and configuration for common errors.
+      #
+      # @param _kitchen_state [::Hash] the mutable Kitchen instance state.
+      # @return [Boolean] +true+ if any errors are found; +false+ if no errors are found.
+      # @see https://github.com/test-kitchen/test-kitchen/blob/v1.21.2/lib/kitchen/verifier/base.rb#L85-L91
+      def doctor(_kitchen_state)
+        false
+      end
+
+      # finalize_config! configures InSpec options which remain consistent between systems.
+      #
+      # @param kitchen_instance [::Kitchen::Instance] an associated Kitchen instance.
+      # @return [self]
+      def finalize_config!(kitchen_instance)
+        super kitchen_instance
+        configure_inspec_connection_options
+        configure_inspec_miscellaneous_options
+      end
+
+      private
+
+      def configure_inspec_connection_options
+        @inspec_options.merge! transport_connection_options
+
+        @inspec_options
+          .store(
+            "connection_timeout",
+            @inspec_options.delete("timeout")
+          )
+      end
+
+      def configure_inspec_miscellaneous_options
+        @inspec_options.merge!(
+          "color" => config_color,
+          "distinct_exit" => false,
+          "sudo" => false,
+          "sudo_command" => "sudo -E",
+          "sudo_options" => "",
+        )
+      end
+
+      def load_outputs(kitchen_state:)
+        ::Kitchen::Util.stringified_hash(Hash(kitchen_state.fetch(:kitchen_terraform_outputs))).tap do |outputs|
+          @system_attrs_resolver = ::Kitchen::Terraform::SystemAttrsResolver.new outputs: outputs
+          @system_hosts_resolver = ::Kitchen::Terraform::SystemHostsResolver.new outputs: outputs
+        end
+      rescue ::KeyError => key_error
+        raise ::Kitchen::Terraform::Error,
+              "Loading Terraform outputs from the Kitchen state failed; this implies that the " \
+              "Kitchen-Terraform provisioner has not successfully converged\n#{key_error}"
+      end
+
+      def initialize(configuration = {})
+        init_config configuration
+        @inspec_options = {}
+        @transport_attributes = [
+          :compression, :compression_level, :connection_retries, :connection_retry_sleep, :connection_timeout,
+          :keepalive, :keepalive_interval, :max_wait_until_ready,
+        ]
+      end
+
+      def inspec_profile_path
+        ::File.join config.fetch(:test_base_path), instance.suite.name
+      end
+
+      # load_needed_dependencies! loads the InSpec libraries required to verify a Terraform state.
+      #
+      # @raise [::Kitchen::ClientError] if loading the InSpec libraries fails.
+      # @see https://github.com/test-kitchen/test-kitchen/blob/v1.21.2/lib/kitchen/configurable.rb#L252-L274
+      def load_needed_dependencies!
+        require "kitchen/terraform/inspec"
+        require "kitchen/terraform/system"
+        ::Kitchen::Terraform::InSpec.logger = logger
+      rescue ::LoadError => load_error
+        raise ::Kitchen::ClientError, load_error.message
+      end
+
+      def transport_connection_options
+        ::Kitchen::Util.stringified_hash(
+          instance.transport.diagnose.select do |key|
+            @transport_attributes.include? key
+          end.tap do |options|
+            options.store :timeout, options.fetch(:connection_timeout)
+          end
+        )
+      end
     end
-  rescue ::Kitchen::Terraform::Error => error
-    raise(
-      ::Kitchen::ActionFailed,
-      error.message
-    )
-  end
-
-  # doctor checks the system and configuration for common errors.
-  #
-  # @param _kitchen_state [::Hash] the mutable Kitchen instance state.
-  # @return [Boolean] +true+ if any errors are found; +false+ if no errors are found.
-  # @see https://github.com/test-kitchen/test-kitchen/blob/v1.21.2/lib/kitchen/verifier/base.rb#L85-L91
-  def doctor(_kitchen_state)
-    false
-  end
-
-  # finalize_config! configures InSpec options which remain consistent between systems.
-  #
-  # @param kitchen_instance [::Kitchen::Instance] an associated Kitchen instance.
-  # @return [self]
-  def finalize_config!(kitchen_instance)
-    super kitchen_instance
-    configure_inspec_connection_options
-    configure_inspec_miscellaneous_options
-  end
-
-  private
-
-  attr_accessor :inspec_options, :outputs, :transport_attributes
-
-  # @api private
-  def configure_inspec_connection_options
-    inspec_options.merge! transport_connection_options
-
-    inspec_options
-      .store(
-        "connection_timeout",
-        inspec_options.delete("timeout")
-      )
-  end
-
-  # @api private
-  def configure_inspec_miscellaneous_options
-    inspec_options.merge!(
-      "color" => config_color,
-      "distinct_exit" => false,
-      "sudo" => false,
-      "sudo_command" => "sudo -E",
-      "sudo_options" => "",
-    )
-  end
-
-  # @api private
-  def load_outputs(kitchen_state:)
-    self.outputs = ::Kitchen::Util.stringified_hash Hash kitchen_state.fetch :kitchen_terraform_outputs
-  rescue ::KeyError
-    raise(
-      ::Kitchen::Terraform::Error,
-      "The Kitchen state does not include :kitchen_terraform_outputs; this implies that the kitchen-terraform " \
-      "provisioner has not successfully converged"
-    )
-  end
-
-  # @api private
-  def initialize(configuration = {})
-    init_config configuration
-    self.inspec_options = {}
-    self.transport_attributes = [:compression, :compression_level, :connection_retries, :connection_retry_sleep,
-                                 :connection_timeout, :keepalive, :keepalive_interval, :max_wait_until_ready]
-  end
-
-  # @api private
-  def inspec_profile_path
-    ::File
-      .join(
-        config.fetch(:test_base_path),
-        instance
-          .suite
-          .name
-      )
-  end
-
-  # load_needed_dependencies! loads the InSpec libraries required to verify a Terraform state.
-  #
-  # @api private
-  # @raise [::Kitchen::ClientError] if loading the InSpec libraries fails.
-  # @see https://github.com/test-kitchen/test-kitchen/blob/v1.21.2/lib/kitchen/configurable.rb#L252-L274
-  def load_needed_dependencies!
-    require "kitchen/terraform/inspec"
-    ::Kitchen::Terraform::InSpec.logger = logger
-  rescue ::LoadError => load_error
-    raise(
-      ::Kitchen::ClientError,
-      load_error.message
-    )
-  end
-
-  # @api private
-  def transport_connection_options
-    return ::Kitchen::Util.stringified_hash(
-             instance.transport.diagnose.select do |key|
-               transport_attributes.include? key
-             end.tap do |options|
-               options.store :timeout, options.fetch(:connection_timeout)
-             end
-           )
-  end
-
-  # @api private
-  # @raise [::Kitchen::Terraform::Error] if running InSpec results in failure.
-  def verify(system:, host:)
-    info "Verifying host '#{host}' of system '#{system.fetch :name}'"
-    inspec_options.store :host, host
-    ::Kitchen::Terraform::InSpecOptionsMapper.new(system: system).map options: inspec_options
-    ::Kitchen::Verifier::Terraform::ConfigureInspecRunnerAttributes.call system: system, options: inspec_options,
-                                                                         outputs: outputs
-    ::Kitchen::Terraform::InSpec.new(options: inspec_options, path: inspec_profile_path).exec
   end
 end
-
-require "kitchen/verifier/terraform/configure_inspec_runner_attributes"
