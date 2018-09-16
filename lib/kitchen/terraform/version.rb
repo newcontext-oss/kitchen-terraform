03a93f77ac1a2f6a9d15049fa06ded0f298906ef
Aaron Lane <aaron.lane@newcontext.com>
2018-09-16 11:25:19 -0400

    Update gem version to 4.0.1

--- lib/kitchen/terraform/version.rb
+++ lib/kitchen/terraform/version.rb
@@ -72,7 +72,7 @@ module ::Kitchen::Terraform::Version
 
     # @api private
     def value
-      self.value = ::Gem::Version.new "4.0.0" if not @value
+      self.value = ::Gem::Version.new "4.0.1" if not @value
       @value
     end
 
