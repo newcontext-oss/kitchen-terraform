8fb277145b8e4091f8eda2d1243409dc42e77f85
Aaron Lane <aarondrewlane@gmail.com>
2018-12-03 04:30:22 +0000

    Update version to 4.0.6

--- spec/support/kitchen/terraform/configurable_examples.rb
+++ spec/support/kitchen/terraform/configurable_examples.rb
@@ -35,7 +35,7 @@ require "support/kitchen/instance_context"
     end
 
     it "equals the gem version" do
-      expect(subject.instance_variable_get(:@plugin_version)).to eq "4.0.5"
+      expect(subject.instance_variable_get(:@plugin_version)).to eq "4.0.6"
     end
   end
 
