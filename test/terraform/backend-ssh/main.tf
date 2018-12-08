3af650add698610f72e48a198c732e0210a04ab5
Aaron Lane <aarondrewlane@gmail.com>
2018-12-08 16:10:14 +0000

    Use community sshd Docker image

--- test/terraform/backend-ssh/main.tf
+++ test/terraform/backend-ssh/main.tf
@@ -3,18 +3,28 @@ provider "docker" {
   version = "~> 1.1"
 }
 
-resource "docker_image" "eg_sshd" {
-  keep_locally = true
-  name         = "eg_sshd"
+data "docker_registry_image" "ubuntu_sshd" {
+  name = "rastasheep/ubuntu-sshd:latest"
+}
+
+resource "docker_image" "ubuntu_sshd" {
+  keep_locally  = true
+  name          = "${data.docker_registry_image.ubuntu_sshd.name}"
+  pull_triggers = ["${data.docker_registry_image.ubuntu_sshd.sha256_digest}"]
 }
 
 resource "docker_container" "host" {
-  image    = "${docker_image.eg_sshd.name}"
+  image    = "${docker_image.ubuntu_sshd.name}"
   must_run = true
   name     = "host"
 
   ports {
     external = 2222
     internal = 22
+  }
+
+  upload {
+    content = "${file("${path.module}/id_ed25519.pub")}"
+    file    = "/root/.ssh/authorized_keys"
   }
 }
