6bbc317078e491cb65f4c7fc501d85d818ba71e3
aaron-lane <aarondrewlane@gmail.com>
2019-01-03 19:08:10 -0500

    Implement Apply

---
+++ lib/kitchen/terraform/command/apply.rb
@@ -0,0 +1,105 @@
+# frozen_string_literal: true
+
+# Copyright 2016 New Context Services, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "kitchen"
+require "kitchen/terraform/command_flag/color"
+require "kitchen/terraform/command_flag/lock_timeout"
+require "kitchen/terraform/command_flag/lock"
+require "kitchen/terraform/command_flag/variable_files"
+require "kitchen/terraform/command_flag/variables"
+require "kitchen/terraform/shell_out_nu"
+
+module Kitchen
+  module Terraform
+    module Command
+      # Apply is the class of objects which represent the <tt>terraform apply</tt> command.
+      class Apply
+        class << self
+          # Initializes an instance by running `terraform apply`.
+          #
+          # @param options [::Hash] the command options.
+          # @option options [true, false] :color a toggle for colored output.
+          # @option options [::String] :directory the directory in which to run the command.
+          # @option options [true, false] :lock a toggle for locking the state.
+          # @option options [::Integer] :lock_timeout the maximum duration in seconds to wait for the lock.
+          # @option options [::Integer] :timeout the maximum duration in seconds to run the command.
+          # @option options [::Array<::String>] :variable_files files containing variables for the configuration.
+          # @option options [::Hash{::String => ::String}] :variables varibales for the configuration.
+          # @raise [::Kitchen::Terraform::Error] if the result of running the command is a failure.
+          # @return [self]
+          # @yieldparam apply [::Kitchen::Terraform::Command::Apply] an instance initialized with the output of the
+          #   command.
+          def run(options)
+            new(options).tap do |apply|
+              ::Kitchen::Terraform::ShellOutNu.run(
+                command: apply,
+                directory: options.fetch(:directory),
+                timeout: options.fetch(:timeout),
+              )
+              yield apply: apply
+            end
+
+            self
+          end
+        end
+
+        def ==(apply)
+          to_s == apply.to_s
+        end
+
+        def store(output:)
+          @output = String output
+
+          self
+        end
+
+        def to_s
+          ::Kitchen::Terraform::CommandFlag::VariableFiles.new(
+            command: ::Kitchen::Terraform::CommandFlag::Variables.new(
+              command: ::Kitchen::Terraform::CommandFlag::LockTimeout.new(
+                command: ::Kitchen::Terraform::CommandFlag::Lock.new(
+                  command: ::Kitchen::Terraform::CommandFlag::Color.new(
+                    command: ::String.new(
+                      "terraform apply " \
+                      "-auto-approve=true " \
+                      "-input=false " \
+                      "-refresh=true"
+                    ),
+                    color: @color,
+                  ),
+                  lock: @lock,
+                ),
+                lock_timeout: @lock_timeout,
+              ),
+              variables: @variables,
+            ),
+            variable_files: @variable_files,
+          ).to_s
+        end
+
+        private
+
+        def initialize(options)
+          @color = options.fetch :color
+          @lock = options.fetch :lock
+          @lock_timeout = options.fetch :lock_timeout
+          @variable_files = options.fetch :variable_files
+          @variables = options.fetch :variables
+        end
+      end
+    end
+  end
+end
