532e4559207c53b6964c23cb283d3ac35d515a54
aaron-lane <aarondrewlane@gmail.com>
2019-04-21 23:31:43 -0400

    Format Provisioner

--- lib/kitchen/provisioner/terraform.rb
+++ lib/kitchen/provisioner/terraform.rb
@@ -18,77 +18,79 @@ require "kitchen"
 require "kitchen/terraform/configurable"
 require "kitchen/terraform/error"
 
-# This namespace is defined by Kitchen.
-#
-# @see http://www.rubydoc.info/gems/test-kitchen/Kitchen/Provisioner
-module ::Kitchen::Provisioner
-end
-
-# The provisioner utilizes the driver to apply changes to the Terraform state in order to reach the desired
-# configuration of the root module.
-#
-# === Commands
-#
-# The following command-line actions are provided by the provisioner.
-#
-# ==== kitchen converge
-#
-# A Test Kitchen instance is converged through the following steps.
-#
-# ===== Selecting the Test Terraform Workspace
-#
-#   terraform workspace select kitchen-terraform-<instance>
-#
-# ===== Updating the Terraform Dependency Modules
-#
-#   terraform get -update <directory>
-#
-# ===== Validating the Terraform Root Module
-#
-#   terraform validate \
-
-#     [-no-color] \
-#     [-var=<variables.first>...] \
-#     [-var-file=<variable_files.first>...] \
-#     <directory>
-#
-# ===== Applying the Terraform State Changes
-#
-#   terraform apply\
-#     -lock=<lock> \
-#     -lock-timeout=<lock_timeout>s \
-#     -input=false \
-#     -auto-approve=true \
-#     [-no-color] \
-#     -parallelism=<parallelism> \
-#     -refresh=true \
-#     [-var=<variables.first>...] \
-#     [-var-file=<variable_files.first>...] \
-#     <directory>
-#
-# === Configuration Attributes
-#
-# The provisioner has no configuration attributes, but the +provisioner+ mapping must be declared with the plugin name
-# within the {http://kitchen.ci/docs/getting-started/kitchen-yml Test Kitchen configuration file}.
-#
-#   provisioner:
-#     name: terraform
-#
-# @example Describe the converge command
-#   kitchen help converge
-# @example Converge a Test Kitchen instance
-#   kitchen converge default-ubuntu
-# @version 2
-class ::Kitchen::Provisioner::Terraform < ::Kitchen::Provisioner::Base
-  kitchen_provisioner_api_version 2
-
-  include ::Kitchen::Terraform::Configurable
-
-  # Converges a Test Kitchen instance.
+module Kitchen
+  # This namespace is defined by Kitchen.
   #
-  # @param _state [::Hash] the mutable instance and provisioner state.
-  # @raise [::Kitchen::ActionFailed] if the result of the action is a failure.
-  def call(_state)
-    instance.driver.apply
+  # @see https://www.rubydoc.info/gems/test-kitchen/Kitchen/Provisioner
+  module Provisioner
+    # The provisioner utilizes the driver to apply changes to the Terraform state in order to reach the desired
+    # configuration of the root module.
+    #
+    # === Commands
+    #
+    # The following command-line actions are provided by the provisioner.
+    #
+    # ==== kitchen converge
+    #
+    # A Test Kitchen instance is converged through the following steps.
+    #
+    # ===== Selecting the Test Terraform Workspace
+    #
+    #   terraform workspace select kitchen-terraform-<instance>
+    #
+    # ===== Updating the Terraform Dependency Modules
+    #
+    #   terraform get -update <directory>
+    #
+    # ===== Validating the Terraform Root Module
+    #
+    #   terraform validate \
+
+    #     [-no-color] \
+    #     [-var=<variables.first>...] \
+    #     [-var-file=<variable_files.first>...] \
+    #     <directory>
+    #
+    # ===== Applying the Terraform State Changes
+    #
+    #   terraform apply\
+    #     -lock=<lock> \
+    #     -lock-timeout=<lock_timeout>s \
+    #     -input=false \
+    #     -auto-approve=true \
+    #     [-no-color] \
+    #     -parallelism=<parallelism> \
+    #     -refresh=true \
+    #     [-var=<variables.first>...] \
+    #     [-var-file=<variable_files.first>...] \
+    #     <directory>
+    #
+    # === Configuration Attributes
+    #
+    # The provisioner has no configuration attributes, but the +provisioner+ mapping must be declared with the plugin name
+    # within the {http://kitchen.ci/docs/getting-started/kitchen-yml Test Kitchen configuration file}.
+    #
+    #   provisioner:
+    #     name: terraform
+    #
+    # @example Describe the converge command
+    #   kitchen help converge
+    # @example Converge a Test Kitchen instance
+    #   kitchen converge default-ubuntu
+    # @version 2
+    class ::Kitchen::Provisioner::Terraform < ::Kitchen::Provisioner::Base
+      kitchen_provisioner_api_version 2
+
+      include ::Kitchen::Terraform::Configurable
+
+      # Converges a Test Kitchen instance.
+      #
+      # @param _state [::Hash] the mutable instance and provisioner state.
+      # @raise [::Kitchen::ActionFailed] if the result of the action is a failure.
+      def call(_state)
+        instance.driver.apply
+      end
+
+    end
   end
 end
