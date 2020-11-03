bd36d3d8c16b32f77c167269fc5016975fc788c8
Kevin Buchs <kevin.buchs@gmail.com>
2020-11-02 18:05:10 -0600

    needed to change bundler config to allow running without gems.locked

--- .github/actions/setup-bundler-for-testing/setup-bundler-for-testing.sh
+++ .github/actions/setup-bundler-for-testing/setup-bundler-for-testing.sh
@@ -3,7 +3,8 @@
 gem install bundler --conservative --minimal-deps --no-document \
   --version="~>2.0"
 bundle config --local gemfile "${GEMFILE_DIR}/gems.rb"
-bundle config --local deployment true
+# this would force a gems.locked file to exist, not for gems!
+# bundle config --local deployment true
 bundle config --local jobs $(nproc --ignore=1)
 bundle config --local set clean true
 bundle config --local set frozen true
