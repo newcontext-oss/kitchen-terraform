5b225c3f380c6414844350c355ff292a1f00b0f5
Kevin Buchs <kevin.buchs@gmail.com>
2020-11-04 08:40:26 -0600

    update bundler configuration to deal with no lock files

--- .github/actions/setup-bundler-for-testing/setup-bundler-for-testing.sh
+++ .github/actions/setup-bundler-for-testing/setup-bundler-for-testing.sh
@@ -3,9 +3,13 @@
 gem install bundler --conservative --minimal-deps --no-document \
   --version="~>2.0"
 bundle config --local gemfile "${GEMFILE_DIR}/gems.rb"
-# this would force a gems.locked file to exist, not for gems!
-# bundle config --local deployment true
 bundle config --local jobs $(nproc --ignore=1)
 bundle config --local set clean true
-bundle config --local set frozen true
 bundle config --local set specific_platform true
+
+# this would force a gems.locked file to exist, not for gems!
+# bundle config --local deployment true
+bundle config --local deployment false
+
+# bundle config --local set frozen true
+bundle config --local set frozen false
