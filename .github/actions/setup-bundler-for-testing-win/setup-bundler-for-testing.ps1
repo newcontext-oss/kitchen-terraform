7dc932cda07c1e88f5e46224d816f9d1ec51e645
Kevin Buchs <kevin.buchs@gmail.com>
2020-12-04 08:07:28 -0600

    change bundler setup GitHub action step to be specific for Windows

---
+++ .github/actions/setup-bundler-for-testing-win/setup-bundler-for-testing.ps1
@@ -0,0 +1,13 @@
+gem install bundler --conservative --minimal-deps --no-document --version="~>2.0"
+$GEMFILEDIR = $Env:GEMFILE_DIR
+bundle config --local gemfile "${GEMFILEDIR}/gems.rb"
+bundle config --local jobs $(nproc --ignore=1)
+bundle config --local set clean true
+bundle config --local set specific_platform true
+
+# this would force a gems.locked file to exist, not for gems!
+# bundle config --local deployment true
+bundle config --local deployment false
+
+# bundle config --local set frozen true
+bundle config --local set frozen false
