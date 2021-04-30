303bfe9b8f9c71a770448a501c22a49dc165cbed
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-04-30 16:25:00 -0400

    Add attributes-post-0-15-0 suite

---
+++ test/terraform/post-0-15-0/attributes/outputs.tf
@@ -0,0 +1,12 @@
+output "first_output" {
+  value = "First Output"
+}
+
+output "second_output" {
+  value     = "Second Output"
+  sensitive = true
+}
+
+output "third_output" {
+  value = "Third Output"
+}
