bc5ab16cff5585e1560272696f9aed86dd80cc5c
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-05-18 21:55:25 -0400

    Refactor Create#call

--- lib/kitchen/terraform/driver/create.rb
+++ lib/kitchen/terraform/driver/create.rb
@@ -49,11 +49,7 @@ module Kitchen
         # @raise [Kitchen::TransientFailure] if a command fails.
         # @return [self]
         def call
-          logger.warn "Reading the Terraform client version..."
-          command_executor.run command: version, options: options do |standard_output:|
-            self.client_version = ::Gem::Version.new standard_output.slice /Terraform v(\d+\.\d+\.\d+)/, 1
-          end
-          logger.warn "Finished reading the Terraform client version."
+          read_client_version
           verify_version.call version: client_version
           initialize_directory
           create_or_select_workspace
@@ -125,6 +121,14 @@ module Kitchen
           ) do |standard_output:|
           end
           logger.warn "Finished initializing the Terraform working directory."
+        end
+
+        def read_client_version
+          logger.warn "Reading the Terraform client version..."
+          command_executor.run command: version, options: options do |standard_output:|
+            self.client_version = ::Gem::Version.new standard_output.slice /Terraform v(\d+\.\d+\.\d+)/, 1
+          end
+          logger.warn "Finished reading the Terraform client version."
         end
 
         def select_workspace
