df8170b510be0692978dc751ed9e2e29b5c3ab7b
Aaron Lane <2400330-aaron-lane@users.noreply.gitlab.com>
2023-02-12 20:23:30 -0500

    Add transport plugin

---
+++ lib/kitchen/transport/terraform.rb
@@ -0,0 +1,92 @@
+# frozen_string_literal: true
+
+# Copyright 2016-2021 Copado NCS LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "kitchen"
+require "kitchen/terraform/config_attribute/client"
+require "kitchen/terraform/configurable"
+require "kitchen/terraform/transport/doctor"
+
+module Kitchen
+  # This namespace is defined by Kitchen.
+  #
+  # @see http://www.rubydoc.info/gems/test-kitchen/Kitchen/Transport
+  module Transport
+    # The transport is responsible for the communication with an instance,
+    # that is Terraform CLI comands.
+    #
+    # === Configuration Attributes
+    #
+    # The configuration attributes of the transport control the behaviour of the Terraform commands that are run.
+    # Within the
+    # {http://kitchen.ci/docs/getting-started/kitchen-yml Test Kitchen configuration file}, these attributes must be
+    # declared in the +transport+ mapping along with the plugin name.
+    #
+    #   tarnsport:
+    #     name: terraform
+    #     a_configuration_attribute: some value
+    #
+    # ==== client
+    #
+    # {include:Kitchen::Terraform::ConfigAttribute::Client}
+    #
+    # === Ruby Interface
+    #
+    # This class implements the interface of Kitchen::Configurable which requires the following Reek suppressions:
+    # :reek:MissingSafeMethod { exclude: [ finalize_config! ] }
+    #
+    # @version 2
+    class Terraform < ::Kitchen::Transport::Base
+      kitchen_transport_api_version 2
+
+      no_parallel_for
+
+      include ::Kitchen::Terraform::ConfigAttribute::Client
+
+      include ::Kitchen::Terraform::Configurable
+
+      # doctor checks the system and configuration for common errors.
+      #
+      # @param state [Hash] the mutable Kitchen instance state.
+      # @return [Boolean] +true+ if any errors are found; +false+ if no errors are found.
+      def doctor(state)
+        ::Kitchen::Terraform::Transport::Doctor.new(
+          instance_name: instance.name,
+          logger: logger
+        ).call config: config
+      end
+
+      # #finalize_config! invokes the super implementation and then initializes the strategies.
+      #
+      # @param instance [Kitchen::Instance] an associated instance.
+      # @raise [Kitchen::ClientError] if the instance is nil.
+      # @return [self]
+      # @see Kitchen::Configurable#finalize_config!
+      def finalize_config!(instance)
+        super instance
+
+        self
+      end
+
+      # #initialize prepares a new instance of the class.
+      #
+      # @param config [Hash] the transport configuration.
+      # @return [Kitchen::Transport::Terraform]
+      def initialize(config = {})
+        super config
+      end
+    end
+  end
+end
---
+++ spec/lib/kitchen/transport/terraform_spec.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+# Copyright 2016-2021 Copado NCS LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "kitchen"
+require "kitchen/transport/terraform"
+require "support/kitchen/terraform/config_attribute/client_examples"
+require "support/kitchen/terraform/configurable_examples"
+
+::RSpec.describe ::Kitchen::Transport::Terraform do
+  subject do
+    described_class.new config
+  end
+
+  let :config do
+    { }
+  end
+
+  let :kitchen_instance do
+    ::Kitchen::Instance.new(
+      driver: ::Kitchen::Driver::Base.new,
+      lifecycle_hooks: ::Kitchen::LifecycleHooks.new(config, state_file),
+      logger: ::Kitchen::Logger.new,
+      platform: ::Kitchen::Platform.new(name: "test-platform"),
+      provisioner: ::Kitchen::Provisioner::Base.new,
+      state_file: state_file,
+      suite: ::Kitchen::Suite.new(name: "test-suite"),
+      transport: subject,
+      verifier: ::Kitchen::Verifier::Base.new,
+    )
+  end
+
+  let :state do
+    {}
+  end
+
+  let :state_file do
+    ::Kitchen::StateFile.new "/kitchen", "test-suite-test-platform"
+  end
+
+  it_behaves_like "Kitchen::Terraform::ConfigAttribute::Client"
+
+  it_behaves_like "Kitchen::Terraform::Configurable"
+
+  describe ".serial_actions" do
+    specify "no actions are returned" do
+      expect(described_class.serial_actions).to be_empty
+    end
+  end
+
+  describe "#doctor" do
+    let :kitchen_instance_state do
+      {}
+    end
+
+    before do
+      subject.finalize_config! kitchen_instance
+    end
+
+    specify "should return true" do
+      expect(subject.doctor(kitchen_instance_state)).to be_truthy
+    end
+  end
+end
